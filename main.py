5048ca6dbd361e9bbd73bc2022527ad138156a53
Derek Perkins <1derek.perkins1@gmail.com>
2024-04-07 22:04:11 -0400

    c

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print()
+import mysql.connector
+connection = mysql.connector.connect(user = 'root', database = 'main', password = '12488970')
+
+cursor = connection.cursor()
+testQuery = (“SELECT * FROM users”)
+cursor.execute(testQuery)
+
+for item in cursor:
+    print(item)
+
+cursor.close()
+connection.close()
+
+def check_balance():
+    pass
+
+def deposit():
+    pass
+    
+def withdraw():
+    pass 
+
+def create_account():
+    pass
+
+def delete_account():
+    pass
+
+def modify_account():
+    pass       
