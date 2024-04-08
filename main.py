2f4944fb07a78652b15296329c4d154948e752e7
Derek Perkins <1derek.perkins1@gmail.com>
2024-04-08 18:32:57 -0400

    h

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import mysql.connector
-connection = mysql.connector.connect(user = 'root', database = 'main', password = '12488970')
+
+connection = mysql.connector.connect(user='root', database='users', password='12488970')
 
 cursor = connection.cursor()
-testQuery = (“SELECT * FROM users”)
-cursor.execute(testQuery)
 
-for item in cursor:
+testQuery = ("SELECT * FROM guy")
+
+cursor.execute(testQuery)
+data = cursor.fetchall()
+
+for item in data:
     print(item)
 
 cursor.close()
 connection.close()
 
-def check_balance():
+
+
+def check_balance(name):
     pass
+
+    #print(val)
+check_balance('guy')
 
 def deposit():
     pass
